c943dc66c5d3ed33fa8af0b1d5e8979b572e6c88
CristianCgu <46873555+CristianCgu@users.noreply.github.com>
2020-01-14 23:08:39 -0500

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,29 @@
+
+
+"""mystring = 'McGill'
+print(mystring)
+print(mystring.lower()) """
+#_________________________________________________________________________________________________________
+"""mystring = 'McGill is my university'
+mystring_split = mystring.split(' ')
+print(mystring_split) """
+#_________________________________________________________________________________________________________
+"""mylist = [1,2.0, 3+4j , "Hello"]                      # lists can take in multiple data types
+print(mylist)
+print(type(mylist)) """
+#__________________________________________________________________________________________________________
+ """mydict = {}                                               # similar to hash maps in Java
+print(type(mydict))
+mydict['myfirstkey'] = 1
+print(mydict['myfirstkey']) """
+#__________________________________________________________________________________________________________
+
+""""def addTwoNumbers (a,b):
+    return a+b
+
+print(addTwoNumbers (5,2)) """
+#_________________________________________________________________________________________________________
+
+mystring = 'Ma Blonde Mange Des Processeurs Et Des Gits'
+a = range(len(mystring))
+print(a)
